684cbe619cb64b326398be36bf813eb6ab687e92
Nick Scofield <c.nick.scofield@gmail.com>
2023-02-02 14:39:57 -0700

    commit tree a82810bf4c1feb7f414fb6f1c49eacab543e8e31

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if (array.length == 1)
+    array.first()
+  else
+    comma = (array.length > 2) ? "," : ""
+    last = array.pop()
+    array.join(", ") << "#{comma} and #{last}"
+  end
 end
